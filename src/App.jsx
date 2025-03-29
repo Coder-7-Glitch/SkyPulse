84b9bcf1f1608456bb63ed961917b1f07d40b9cc
Muhammad Ahad <ahad97140@gmail.com>
2025-03-29 23:00:46 +0500

    Files deleted

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <>
       <Header />
-      <Security />
+      {/* <Security /> */}
       <div className="lg:flex w-full">
         <Sidebar />
         <Routes>
--- src/Pages/EarningPage.jsx
+++ src/Pages/EarningPage.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Earnings from '../components/Earning/Earnings'
 
 export default function EarningPage() {
   return (
-    <div>
-      
+    <div className="w-full lg:px-12 ps-[4rem] pe-[1rem] mb-3">
+      <div className="pt-3">
+        <div className="heading">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl text-text_Color">
+            Earnings
+          </h1>
+          <p className="text-sm md:text-lg text-[#9d9d9d] mt-1">
+            With all of the styling tool options available in today's market
+          </p>
+        </div>
+        <Earnings />
+      </div>
     </div>
-  )
+  );
 }
